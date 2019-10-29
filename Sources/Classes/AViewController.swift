86d53b2284e940a284578aa0ecf90ead480d8c55
zowell <zowell@lvhanglong.local>
2019-10-29 14:41:36 +0800

    sss

---
+++ Sources/Classes/AViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AViewController.swift
+//  A_Swift
+//
+//  Created by zowell on 2019/10/29.
+//  Copyright © 2019 Chancc. All rights reserved.
+//
+
+import UIKit
+
+class AViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
